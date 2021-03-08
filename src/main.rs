dccd20c8077c06743ad07df9b8dbda2f8063d728
bishtpawan <pawan.bisht@knoldus.com>
2021-03-08 15:31:15 +0530

    Implement VGA buffer in Custom OS

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@
 #![no_std]
 // Disabling default entry points
 #![no_main]
-
+mod vga_buffer;
 use core::panic::PanicInfo;
+use core::fmt::Write;
+
 static PRINT_MESSAGE: &[u8] = b"WELCOME TO OUR CUSTOM OS";
 
 // This function invoked by the compiler when a panic occurs
 #[panic_handler]
-fn panic(_panic_info: &PanicInfo) -> ! {
+fn panic(panic_info: &PanicInfo) -> ! {
+    println!("{}", panic_info);
     loop {}
 }
 
@@ -19,17 +22,28 @@ fn panic(_panic_info: &PanicInfo) -> ! {
 // we have used is the extern "C", this is used to tell the compiler
 // to use the C calling convention for this function.
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_data();
 
-    let buffer = 0xb8000 as *mut u8;
+    loop {}
+}
+
+
+
+
+
+/*let buffer = 0xb8000 as *mut u8;
 
     for (num, &byte) in PRINT_MESSAGE.iter().enumerate() {
         unsafe {
             *buffer.offset(num as isize * 2) = byte;
             *buffer.offset(num as isize * 2 + 1) = 0x9;
         }
-    }
+    }*/
 
-    // The goal is just to overwrite the entry point so we just looped indefinitely
-    loop {}
-}
-
+//vga_buffer::print_something();
+//use core::fmt::Write;
+//vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+//write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+/*println!("Hello World{}", "!");
+panic!("Some panic message");
+*/
